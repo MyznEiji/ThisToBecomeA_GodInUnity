e4a134105a922ea89dfc50d01e4aa7e42d237a04
MyznEiji <em711th@icloud.com>
2017-11-11 02:50:08 +0900

    TheConceptAntStructureProject完了,MakeUpTheWorldProjectのScene新規作成

---
+++ Assets/Scripts/TheConceptAndStructure/DropTofu.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DropTofu : MonoBehaviour 
+{
+	[SerializeField] private GameObject tofu;
+	float time = 0;
+	int tofuCount = 0;
+	void Update () 
+	{
+		time += Time.deltaTime;
+		if(time > 2.0f && tofuCount <= 100)
+		{
+			InstantiateTofu();
+		}
+	}
+
+	void InstantiateTofu()
+	{
+		//100個のtofuを生成
+		Instantiate(tofu,Vector3.up * 5,Quaternion.identity);
+		tofuCount ++;
+		time = 0;
+	}
+}
