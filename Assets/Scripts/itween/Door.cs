4df4660ba015994b1578f8ba376a911ce6d44cd5
MyznEiji <em711th@icloud.com>
2017-11-12 18:49:15 +0900

    BasicScript完了 AnimationAndCharacterSceneを新規作成

---
+++ Assets/Scripts/itween/Door.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour 
+{
+	bool opn = false;
+	//void Start () 
+	//{
+	//	
+	//}
+	
+	void Update () 
+	{
+		if(Input.GetMouseButtonDown(0))
+		{
+			DoorOpen();
+		}
+	}
+
+	void DoorOpen()
+	{
+		opn = !opn;
+		if(opn)
+		{
+			RotateJoint(30f,"easeOutBack");
+		}
+		else 
+		{
+			RotateJoint(0f,"easeInQuad");
+		}
+	}
+
+	void RotateJoint(float x,string y)
+	{
+		iTween.RotateTo
+		(
+			gameObject,
+			iTween.Hash("y",x,"islocal",true,"easetype",y)
+		);
+	}
+}
