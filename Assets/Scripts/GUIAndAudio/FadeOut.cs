a85d1bb447f3a240f672c4f61d5b21a65decf52d
MyznEiji <em711th@icloud.com>
2017-11-13 21:52:50 +0900

    fadeOut昨日追加

---
+++ Assets/Scripts/GUIAndAudio/FadeOut.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeOut : MonoBehaviour 
+{
+	public float speed = 0.01f; //透明化の速さ
+	float alfa;
+	float red,green,blue;
+	Color color;
+	void Start () 
+	{
+		red = GetComponent<Image>().color.r;
+		green = GetComponent<Image>().color.g;
+		blue = GetComponent<Image>().color.b;
+		color = GetComponent<Image>().color; 
+	}
+	
+	void Update () 
+	{
+		GetComponent<Image>().color = new Color(red,green,blue,alfa);
+		alfa += speed;	
+	}
+	// IEnumerator ChangeColor()
+	// {	
+	// 	while(true)
+	// 	{
+	// 		color = new Color(red,green,blue,alfa);
+	// 		alfa -= speed;
+	// 		yield return alfa == 0;
+	// 	}
+
+	// 	color = new Color(red,green,blue,alfa);
+	// 	alfa += speed;
+	// }
+}
