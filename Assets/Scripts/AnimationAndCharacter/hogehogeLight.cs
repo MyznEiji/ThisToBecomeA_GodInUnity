5625d01263ca1791db710a9e1edd5a4de5579759
MyznEiji <em711th@icloud.com>
2017-11-13 21:00:52 +0900

    Animation完了 GUIto

---
+++ Assets/Scripts/AnimationAndCharacter/hogehogeLight.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hogehogeLight : MonoBehaviour 
+{
+	public float lightIntensity = 0.2f;
+	Light myLight;
+	void Start () 
+	{
+		myLight = GetComponent<Light>();
+		myLight.intensity = 3;
+	}
+	
+	void Update () 
+	{
+		myLight.intensity = lightIntensity;	
+	}
+}
